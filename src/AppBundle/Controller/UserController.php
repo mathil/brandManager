de558e65ed50097dd5431243c7059e1c0a02d6b0
panhilson <panhilson@gmail.com>
2018-01-27 11:53:01 +0100

    Users list

---
+++ src/AppBundle/Controller/UserController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+/**
+ * User controller.
+ *
+ * @Route("user")
+ */
+class UserController extends Controller
+{
+    /**
+     * @Route("/list", name="bm_user_list")
+     */
+    public function indexAction()
+    {
+
+        $em = $this->getDoctrine()->getManager();
+
+        return $this->render('user/index.html.twig', [
+            'users' => $em->getRepository('AppBundle:User')->findAll()
+        ]);
+    }
+
+}
